0450253ea60b2e587776dace2f8e53f199b66510
Fabian Dingemans <mail@fabiandingemans.nl>
2025-04-16 13:23:07 +0200

    Loading product types and products from db

--- components/quotation/product-finder/ProductFinder.vue
+++ components/quotation/product-finder/ProductFinder.vue
@@ -1,17 +1,24 @@
 <script setup lang="ts">
+    import {useAuthStore} from "~/store/auth";
+    import {useToastStore} from "~/store/toast";
+
     defineProps({
         onProductAdd: {
             type: Function,
             required: true
         }
     });
+    const config = useRuntimeConfig();
+    const { toast } = useToastStore();
+    const authStore = useAuthStore();
+    let backendBaseUrl = config.public.backendBaseUrl;
 
     const toCurrency = (value: string) => {
         return value.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' });
     }
 
-    import {fakeProductTypes} from './tempData/productTypes';
-    const productTypes = fakeProductTypes;
+    const productTypes = ref([]);
+    const products = [];
 
     const selectedProductType = ref('All Product Types');
     const filterMenu = ref(false);
@@ -26,6 +33,77 @@
     const endDateFilter = ref('');
     const priceRangeFilter = ref([0, 1000]);
     const minPersonCountFilter = ref(1);
+
+    const fetchProductTypes = () => {
+        $fetch(backendBaseUrl + '/product-type', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + authStore.jwtToken
+            }
+        })
+            .then(res => {
+                if(!res.success){
+                    toast(res.message, 'error');
+                    return;
+                }
+
+                productTypes.value.push({
+                    value: -1,
+                    title: "All Product Types",
+                });
+
+                res.productTypes.forEach(productType => {
+                    let translation = productType.translations.find(translation => translation.langIsoCode === "en");
+
+                    productTypes.value.push({
+                        value: productType.id,
+                        title: translation.name,
+                    });
+                });
+            }).catch(err => {
+                toast('Error getting product types', 'error');
+            });
+    }
+    const fetchProducts = () => {
+        $fetch(backendBaseUrl + '/product', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Bearer " + authStore.jwtToken
+            }
+        })
+            .then(res => {
+                if(!res.success){
+                    toast(res.message, 'error');
+                    return;
+                }
+
+                res.products.forEach(product => {
+                    let translation = product.translations.find(translation => translation.langIsoCode === "en");
+                    let dates = product.dates;
+
+                    let minPrice = Math.min(...dates.map(date => date.price));
+                    let maxPrice = Math.max(...dates.map(date => date.price));
+
+                    products.push({
+                        id: product.id,
+                        name: translation.name,
+                        description: translation.description,
+                        type: translation.type,
+                        tags: translation.tags,
+                        startLocation: product.startLocation,
+                        endLocation: product.endLocation,
+                        minPrice: minPrice,
+                        maxPrice: maxPrice
+                    });
+                });
+
+                console.log(products);
+            }).catch(err => {
+            toast('Error getting product types', 'error');
+        });
+    }
 
     const resetFilters = () => {
         // Close the filter menu
@@ -51,7 +129,8 @@
         minPersonCountFilter.value = minPersonCountFilterTemp.value;
     }
 
-    const items = fakeProducts;
+    fetchProductTypes();
+    fetchProducts();
 </script>
 
 <template>
@@ -185,7 +264,7 @@
             style="height: calc(100vh - 450px); overflow-y: auto;"
         >
             <v-list-item
-                v-for="(item, index) in filteredItems"
+                v-for="(item, index) in products"
                 :key="index"
                 class="px-0"
             >
