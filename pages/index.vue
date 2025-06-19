4da40959a54e40114efbabdd413819b4a5f85936
Ihavenojoy <tom.van.de.lest@gmail.com>
2025-06-19 09:33:34 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,23 @@
     definePageMeta({
         middleware: 'auth',
     });
+    import {useNavbar} from "~/store/useNavbar";
+    const { navigationItems, signIn, signOut } = useNavbar();
+
 </script>
 
 <template>
-
+  <v-container class="d-flex justify-center align-center" style="height: 100vh;">
+    <v-row class="text-center" justify="center" align="center">
+      <v-col
+          v-for="(item, index) in navigationItems"
+          :key="index"
+          cols="auto"
+      >
+        <v-btn :to="item.to" color="primary" large>
+          {{ item.label }}
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
